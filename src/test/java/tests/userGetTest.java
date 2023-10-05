017b740a68e29369351f2b828ffad14108d954b3
NessiePiotr <nattan-s@yandex.ru>
2023-10-05 17:59:29 +0400

    Ex16: Get Another User Details Auth User

---
+++ src/test/java/tests/userGetTest.java
@@ -0,0 +1,68 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import org.junit.jupiter.api.Test;
+
+import lib.ApiCoreRequests;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class userGetTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
+    @Test
+    public void testGetUserDataNotAuth(){
+        Response responseUserData = RestAssured
+                .get("https://playground.learnqa.ru/api/user/2")
+                .andReturn();
+
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotField(responseUserData, "firstName");
+        Assertions.assertJsonHasNotField(responseUserData, "lastName");
+        Assertions.assertJsonHasNotField(responseUserData, "email");
+    }
+
+    @Test
+    public void testGetUserDetailsAuthUser(){
+        Map<String, String> authData = new HashMap();
+
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password","1234");
+
+        Response responseGetAuth = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
+
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+
+        Response responseUserData = apiCoreRequests.makeGetRequest("https://playground.learnqa.ru/api/user/2", header, cookie);
+
+        String[] expectedFields = {"username", "firstName", "lastName", "email"};
+        Assertions.assertJsonHasFields(responseUserData, expectedFields);
+    }
+
+    @Test
+    public void testGetAnotherUserDetailsAuthUser(){
+        Map<String, String> authData = new HashMap();
+
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password","1234");
+
+        Response responseGetAuth = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
+
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+
+        Response responseUserData = apiCoreRequests.makeGetRequest("https://playground.learnqa.ru/api/user/1", header, cookie);
+
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotField(responseUserData, "firstName");
+        Assertions.assertJsonHasNotField(responseUserData, "lastName");
+        Assertions.assertJsonHasNotField(responseUserData, "email");
+    }
+
+}
